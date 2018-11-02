5cd36039f108389300bd2893d10f36951311c073
SANGET <AlexZhang@Alexs-iMac.local>
2018-11-02 13:12:28 +0800

    fix

--- src/template-engine/for-report/report-generator.js
+++ src/template-engine/for-report/report-generator.js
@@ -42,7 +42,7 @@ export function GeneralReportRender(Action, passProps = {}) {
           keyMapper={this.keyMapper}
           conditionOptions={this.conditionOptions}
           needCount={this.needCount}
-          actionBtns={this.actionBtns}
+          actionBtns={this.reportActionBtns}
           ref={e => this.ReportRef = e}
 
           {...passProps}
