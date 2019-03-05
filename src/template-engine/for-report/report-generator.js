baaf65c18a4fcbf72d6dba42016d23bdc1e78c3d
Jason Zhang <jason.zhang@9street.org>
2019-03-05 17:49:19 +0800

    权限数据统一由顶层组件属性传入

--- src/template-engine/for-report/report-generator.js
+++ src/template-engine/for-report/report-generator.js
@@ -27,7 +27,7 @@ export function GeneralReportRender(Action, passProps = {}, TemplatEngin = Repor
   return class C extends Action {
     /** 用于记录所有的按钮 */
     powerFilterForBtn(btnId) {
-      const { powerMapper } = this;
+      const { powerMapper } = this.props;
       if(!powerMapper) return true;
       const pageName = window.location.hash.replace(/[^a-zA-Z]/g, '');
       return powerMapper.indexOf(`${pageName}_${btnId}`) > -1;
