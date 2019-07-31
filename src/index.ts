240108aa87c9714530fb1c7fd6177e728e75ae8c
SANGET <AlexZhang@Alexs-iMac.local>
2019-07-31 18:25:10 +0800

    feat: export ActionAgent

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import AdminWebScaffold from './layout';
 import { Link } from './router-multiple';
+import ActionAgent from './action-agent';
 
 export * from './config';
 
 export {
-  AdminWebScaffold, Link
+  AdminWebScaffold, Link, ActionAgent
 };
