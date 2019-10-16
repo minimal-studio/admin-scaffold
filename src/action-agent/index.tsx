5e454e6ab160a30f4ba2975b33fe28c3d4d1060a
sanget <zh.sanget@gmail.com>
2019-10-16 08:11:12 +0800

    Merge

--- src/action-agent/index.tsx
+++ src/action-agent/index.tsx
@@ -78,13 +78,8 @@ class ActionAgent<P = {}, S = {}> extends Component<P, S> {
    * @param {agentOptions} object 此方法的配置项
    * @return {async function} 返回传入的第一个参数的包装方法，在此过程插入一些生命周期函数
    */
-<<<<<<< HEAD
-  reqAgent<APIRetrue>(
-    reqFunc: ReqAgentAPI, agentOptions: AgentOptions
-=======
   reqAgent<APIRetrue = any>(
     reqFunc: Function, agentOptions: AgentOptions
->>>>>>> d6479bb68cbda6d8aee155cb332a72d7afaa16df
   ) {
     if (!IsFunc(reqFunc)) {
       const errMsg = 'should pass func at arguments[0]';
