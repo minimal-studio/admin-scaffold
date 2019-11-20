69f4dc2cd155b61b6843eee0529329829f06c361
sanget <zh.sanget@gmail.com>
2019-11-20 17:04:30 +0800

    Add alerts page

--- website-dev/packages/pages/ui-components-demo/alert.tsx
+++ website-dev/packages/pages/ui-components-demo/alert.tsx
@@ -6,7 +6,43 @@ export const Alerts = () => {
     <CardContainer>
       <Card>
         <div className="alerts">
-          <Alert title="Alert" />
+          <h3 className="ps10">Basic Usage</h3>
+          <Alert title="Alert success" type="success" />
+          <Alert title="Alert error" type="error" />
+          <Alert title="Alert warn" type="warn" />
+          <Alert title="Alert normal" type="normal" />
+        </div>
+      </Card>
+      <hr />
+      <Card>
+        <div className="alerts">
+          <h3 className="ps10">With text</h3>
+          <Alert title="Alert success" type="success" text="with text" />
+        </div>
+      </Card>
+      <hr />
+      <Card>
+        <div className="alerts">
+          <h3 className="ps10">With texts</h3>
+          <Alert title="Alert success" type="success"
+            texts={[
+              "desc1",
+              "desc2",
+              "desc3",
+            ]}
+          />
+        </div>
+      </Card>
+      <hr />
+      <Card>
+        <div className="alerts">
+          <h3 className="ps10">Collapsable</h3>
+          <Alert
+            title="Alert success"
+            collapse
+            type="success"
+            text="with text"
+          />
         </div>
       </Card>
     </CardContainer>
